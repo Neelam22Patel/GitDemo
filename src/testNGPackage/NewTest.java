15e01180e8c7a6a4e3455dc5d86cc801e6c0616e
payto <neelam.uvbm@gmail.com>
2020-10-18 20:02:08 -0400

    This is my first commit

---
+++ src/testNGPackage/NewTest.java
@@ -0,0 +1,20 @@
+package testNGPackage;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+ 
+	@Test(priority=2)
+	public void f() {
+			
+		
+  }
+	@Test(priority=1,dependsOnMethods= {"f"},alwaysRun=true)
+	public void b() {
+		System.out.println("there is line on top");
+	}
+}
